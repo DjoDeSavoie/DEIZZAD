b3f7faf8c15350cb56b93fb91c74381a6533a9dd
DjoDeSavoie <jonathandegrange123@gmail.com>
2023-10-20 15:04:03 +0200

    Image character

---
+++ src/main/Game.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Game {
+	
+	private GamePanel gamePanel;
+	
+	public Game() {
+		gamePanel = new GamePanel();
+		new GameWindow(gamePanel);
+		gamePanel.requestFocus();		
+	}
+
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,72 @@
+package main;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.Buffer;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+public class GamePanel extends JPanel {
+
+	private MouseInputs mouseInputs;
+	private int xDelta = 100, yDelta = 100;
+	private BufferedImage image, img;
+	private BufferedImage[] Idle_anim;
+
+	public GamePanel() {
+		mouseInputs = new MouseInputs(this);
+		importImage();
+		loadAnimation();
+
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+	}
+
+	private void importImage() {
+		InputStream stream = getClass().getResourceAsStream("../data/player_sprites.png");
+		try {
+			image = ImageIO.read(stream);
+		} catch (IOException e) {
+			System.out.print("bonjour");
+			e.printStackTrace();
+		}
+	}
+
+	private void loadAnimation() {
+		Idle_anim = new BufferedImage[6];
+
+	}
+
+	public void changeXDelta(int value) {
+		this.xDelta += value;
+		repaint();
+	}
+
+	public void changeYDelta(int value) {
+		this.yDelta += value;
+		repaint();
+	}
+	
+	public void setRectPos(int x, int y) {
+		this.xDelta = x;
+		this.yDelta = y;
+		repaint();
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		img = image.getSubimage(6*60, 7*100, 100, 100);
+		g.drawImage(img, (int) xDelta, (int) yDelta, null);
+		
+		//g.fillRect(xDelta, yDelta, 200, 50);
+
+	}
+
+}
